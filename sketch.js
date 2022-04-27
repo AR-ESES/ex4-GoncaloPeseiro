9138a3425c5ecb69e365be22c9bff3fbb19691b4
GoncaloPeseiro <102664610+GoncaloPeseiro@users.noreply.github.com>
2022-04-27 22:58:04 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,13 +11,42 @@ function setup() {
 }
 
 function draw() {
-  
+
   for (let i = 1; i < espLarg; i++) {
     for (let j = 1; j < espAltura; j++) {
       push();
       translate(i * width / espLarg,j * height / espAltura)
-      circle(0, 0, height / espAltura);
+   translate(-25,-20)
+      scale(0.20);
+      noStroke();
+    fill(51);
+rect(400,200,300,400);
+fill('#fae');
+circle(width / 3.5, height / 8, height / 4);
+line(450,600,450,height);
+line(650,600,650,height);
+fill('#222222');
+rect(700,300,200,100);
+fill('#222222');
+rect(200,300,200,100);
+circle(600, 80, 40);
+circle(500, 80, 40);
+fill('red');
+rect(495, 150, 110, 40);
+fill(51);
+circle(900, 350, 100);
+circle(200, 350, 100);
+
+   
+
+}
+
+
+  
+
       pop();
     }
   }
 }
+
+Conversar em #boas-vindas-e-regras
